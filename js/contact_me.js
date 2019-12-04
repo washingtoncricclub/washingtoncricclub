c543e191e75157b0dcc1da0e3e4a387c62a98d9c
Moiz Hussain <moizhussain.92@gmail.com>
2019-12-04 01:22:22 -0800

    updating formspree url

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "https://formspree.io/washingtoncricclub@gmail.com",
+                url: "https://formspree.io/mjvkodnd",
                 type: "POST",
                 data: {
                     name: name,
