e989d9d440991f22ae241392655bfeb6395014c8
Sunny's <sunnys@Admins-MacBook-Pro.local>
2019-12-03 21:55:07 -0800

    profiles mapped to cricclub

--- js/agency.js
+++ js/agency.js
@@ -8,9 +8,9 @@
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
+            $('html, body').stop().animate({
+                scrollTop: ($($anchor.attr('href')).offset()) && $($anchor.attr('href')).offset().top
+            }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
 });
